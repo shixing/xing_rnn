1c9870da5be8cdd9356f17c23b3065b857383091
Shi Xing <shixing19910105@gmail.com>
2017-09-12 16:40:54 -0700

    a

--- Seq2Seq/py/util/generate_jobs_claim2title_small.py
+++ Seq2Seq/py/util/generate_jobs_claim2title_small.py
@@ -4,8 +4,8 @@ from job import Jobs
 
 def main():
     
-    j = Jobs("claim2title",hours = 10, machine_type = "gpu4")
-    grids = {"name":["c2t"],
+    j = Jobs("claim2title_small",hours = 10, machine_type = "gpu4")
+    grids = {"name":["c2tsmall"],
              "batch_size":[80],
              "size": [500],
              "dropout":[0.8],
@@ -19,11 +19,11 @@ def main():
              "max_source_length":[200],
              "min_target_length":[0],
              "max_target_length":[100],
-             "n_bucket":[5],
+             "n_bucket":[1],
              "optimizer":["sgd"],
              "learning_rate_decay_factor":[0.5],
              "NN":["00001,22223"],
-             "attention_style":["additive"],
+             "attention_style":["multiply"],
              "attention_scale":[False]
     }
 
